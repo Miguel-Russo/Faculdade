3b63f82e398325bc1a888d4318dae1e97e716b73
Miguel-Russo <miguelrusso1819@gmail.com>
2024-12-31 17:08:20 -0300

    Add files via upload

---
+++ 2° Semestre - 2024_2/Linguagem de Programação/Atividade 3/Exercício 7.js
@@ -0,0 +1,8 @@
+let Num = parseInt(prompt("Digite um número: "));
+
+if(Num % 2 == 0){
+    alert("Par");
+}
+else{
+    alert("Ímpar");
+}
---
+++ 2° Semestre - 2024_2/Linguagem de Programação/Atividade 3/Exercício 8.js
@@ -0,0 +1,8 @@
+const funcaoSeta = (X) > X**3;
+const funcaoAnonimo = function(X){return X**3}
+
+let resultadoSeta = funcaoSeta(42);
+let resultadoAnonimo = funcaoAnonimo(42);
+
+console.log(resultadoSeta);
+console.log(resultadoAnonimo);
